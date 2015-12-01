4f646c93ca5ef6c7d3d5913e88b02bb6324dae5c
g. nicholas d'andrea <gnidan@dramafever.com>
2015-11-30 23:05:32 -0500

    Add monster to details

--- app/scripts/components/details.jsx
+++ app/scripts/components/details.jsx
@@ -6,11 +6,33 @@ class Details extends React.Component {
     }
 
     render() {
+        var monster = [
+'',
+'yN',
+'  GB',
+'  IaJEz',
+'Eqpraz',
+'DassC',
+'  GaH',
+'    X',
+''
+]
+        var lines = monster.map(function (line) { return (
+            <pre className='monsters-typeface'>{line}</pre>
+           ); });
         return (
             <div className="details-wrapper">
                 <div className="details">
+                    <header>
                     <h1>HOTMONSTERS.ORG</h1>
-                    <p>december 18, monsterdelphia, monstervania</p>
+                    </header>
+                    <div>
+                    {lines}
+                    </div>
+                    <footer>
+                    <p>friday, december 18, 2015</p>
+                    <p>monsterdelphia, monstervania</p>
+                    </footer>
                 </div>
             </div>
         )
