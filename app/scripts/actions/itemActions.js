87b5e7c607a63a381a17d8fa2894a6b334b975e1
g. nicholas d'andrea <gnidan@dramafever.com>
2015-12-01 15:36:03 -0500

    stella take 3

--- app/scripts/actions/itemActions.js
+++ app/scripts/actions/itemActions.js
@@ -17,7 +17,14 @@ ItemActions.loadItems.listen(function(){
         ['  IJ', ' IaJ', ' STU', ' s$sE E' , ' a0a0a0B', ' aaaaaaa1', ' OP      OP'],
         ['    NMJ', '    b b b b b ', '    !!!', '  yAsD1', '  yCDCz', '    STU ',   '    aaa', '    K  L'],
         ['AaaaaB', 'Aabb&bbaB', 'AIJIJB ', 'AKLKLB', ' STTTU ', 'AaaaaB', ' d  d  d', ' e  e  e'],
-        [" y3aaaaaaF",  "  EFbSTUbccc00", " bcsscbcsbbbsbbbccc00"," bcKLcbcccccc00300", " bcIJcbcD0CD0C"," bcRRcbcaaaaaaF"," bc00cbcaaaaaaH", " cO Pc  f  f"]
+["y3aaaaaaF",
+" EFbSTUbccc00",
+"bcsscbcsbbbsbbbccc00",
+"bcKLcbcccccc00300",
+"bcIJcbcD0CD0C",
+"bcRRcbcaaaaaaF",
+"bc00cbcaaaaaaH",
+"cO Pc  f  f"]
     ];
     this.completed(items);
 
