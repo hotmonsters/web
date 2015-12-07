106ceaf96b936779116809eb69c023788af5a354
g. nicholas d'andrea <gnidan@dramafever.com>
2015-12-07 02:44:49 -0500

    Add key for editor guide

--- app/scripts/components/guide.jsx
+++ app/scripts/components/guide.jsx
@@ -6,7 +6,7 @@ class Guide extends React.Component {
         var glyphs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', '1', '2', '3', '4', '5', '6', '7', '8', '0', '!', '#', '$', '%', '&'];
 
         var items = glyphs.map( (item, index) =>
-                <div className="guide-glyph">
+                <div key={index} className="guide-glyph">
                     <div className="monster-typeface">
                         {item}
                     </div>
